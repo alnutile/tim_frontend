e8e2e0bd0a978c462e0eef9a69e53c188815e390
alnutile <alfrednutile@gmail.com>
2014-04-13 11:19:22 -0400

    Testing

---
+++ app/js/json/people.js
@@ -0,0 +1,20 @@
+[
+    '{{repeat(5, 7)}}',
+    {
+        id: '{{guid()}}',
+        firstname: '{{firstName()}}',
+        lastname: '{{surname()}}',
+        phone: '{{phone()}}',
+        email: '{{email()}}',
+        description: '{{lorem(2)}}',
+        prefix: 'Mr.',
+        middle: '',
+        date_of_birth: '{{date()}}',
+        city: '{{city()}}',
+        zip: '',
+        secondary_phone: '',
+        address1: '{{street()}}',
+        address2: '',
+        state: '{{state()}}'
+    }
+]
